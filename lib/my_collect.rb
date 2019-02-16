7ceaedb9ff6f4978ff14b40f5b4d2f1c03af3ed8
Doug Ward <ward92@gmail.com>
2019-02-16 15:26:58 -0800

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,11 @@
-
-
+def my_collect(collection)
+  if block_given?
+    i = 0
+    new_array = []
+    while i < collection.length
+      new_array << yield(collection[i])
+      i += 1
+    end
+  end
+  new_array
+end
